39f9016b4b6e1de4544f6852f206d3e7ece54920
polarzero <0xpolarzero@gmail.com>
2023-02-24 15:17:30 +0100

    Add useSwarm

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,8 @@
 import useAudio from './useAudio';
 import useConfig from './useConfig';
 import useSpinamp from './useSpinamp';
+import useSwarm from './useSwarm';
 
-const stores = { useAudio, useConfig, useSpinamp };
+const stores = { useAudio, useConfig, useSpinamp, useSwarm };
 
 export default stores;
