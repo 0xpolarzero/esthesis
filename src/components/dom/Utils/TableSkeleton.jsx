2c1ed77d6a370dd0f8bb57c5cbb1a87ba270698f
polarzero <0xpolarzero@gmail.com>
2023-02-24 15:17:22 +0100

    Add TableSkeleton

---
+++ src/components/dom/Utils/TableSkeleton.jsx
@@ -0,0 +1,31 @@
+import { Skeleton, Table } from 'antd';
+
+const TableSkeleton = ({ colCount, rowCount, style, noHeader = false }) => {
+  const columns = Array(colCount)
+    .fill()
+    .map((_, i) => ({
+      title: noHeader ? null : (
+        <Skeleton active title={false} paragraph={{ rows: 1 }} />
+      ),
+      dataIndex: i,
+      key: i,
+      render: () => <Skeleton active title={false} paragraph={{ rows: 1 }} />,
+    }));
+
+  const rows = Array(rowCount)
+    .fill()
+    .map((_, i) => ({
+      key: i,
+    }));
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={rows}
+      style={style}
+      pagination={false}
+    />
+  );
+};
+
+export default TableSkeleton;
--- src/components/dom/Utils/index.js
+++ src/components/dom/Utils/index.js
@@ -1,9 +1,11 @@
 import Duration from './Duration';
 import ElapsedTime from './ElapsedTime';
+import TableSkeleton from './TableSkeleton';
 
 const Utils = {
   Duration,
   ElapsedTime,
+  TableSkeleton,
 };
 
 export default Utils;
