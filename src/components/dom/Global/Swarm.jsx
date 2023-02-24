77d7df02c0f0a50cd0481575f08c194fff02e8fd
polarzero <0xpolarzero@gmail.com>
2023-02-24 15:17:49 +0100

    Update swarm panel style & replace library -> panel

--- src/components/dom/Global/Swarm.jsx
+++ src/components/dom/Global/Swarm.jsx
@@ -1,5 +1,32 @@
+import { Collapse } from 'antd';
+import hooks from '@/hooks';
+import { AiOutlineLeft } from 'react-icons/ai';
+
+const { Panel } = Collapse;
+
 const Swarm = () => {
-  return null;
+  const { isMobile } = hooks.useWindowSize();
+
+  return (
+    <div className='swarm'>
+      <Collapse
+        bordered={false}
+        defaultActiveKey={['1']}
+        expandIcon={() => <AiOutlineLeft size={14} />}
+        expandIconPosition='end'>
+        <Panel
+          //   ref={ref}
+          header='swarm'
+          key='1'
+          className={`panel ${isMobile ? 'mobile' : 'desktop'}`}>
+          content
+          <br />
+          content
+          <br />
+        </Panel>
+      </Collapse>
+    </div>
+  );
 };
 
 export default Swarm;
