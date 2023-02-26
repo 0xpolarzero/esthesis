6ed620267d2f217c45d1e452817ccce3c4a895a3
polarzero <0xpolarzero@gmail.com>
2023-02-26 22:55:58 +0100

    Add orbitcontrols

--- src/components/canvas/Experience.jsx
+++ src/components/canvas/Experience.jsx
@@ -1,10 +1,11 @@
+import { OrbitControls } from '@react-three/drei';
 import Entity from './Entity';
 
 const Experience = () => {
   return (
     <>
       <Entity />
-
+      <OrbitControls />
     </>
   );
 };
