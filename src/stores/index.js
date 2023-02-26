3df823e569e525fe3def2e4998a12d48fd0e120a
polarzero <0xpolarzero@gmail.com>
2023-02-26 22:58:55 +0100

    Add useShare

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,9 @@
 import useAudio from './useAudio';
 import useConfig from './useConfig';
+import useShare from './useShare';
 import useSpinamp from './useSpinamp';
 import useSwarm from './useSwarm';
 
-const stores = { useAudio, useConfig, useSpinamp, useSwarm };
+const stores = { useAudio, useConfig, useShare, useSpinamp, useSwarm };
 
 export default stores;
