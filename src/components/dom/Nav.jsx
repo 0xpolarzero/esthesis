56f81b0ea871f9bf0673eb1b3b431d540b0c4027
polarzero <0xpolarzero@gmail.com>
2023-02-26 22:57:00 +0100

    Update display in shared page

--- src/components/dom/Nav.jsx
+++ src/components/dom/Nav.jsx
@@ -2,26 +2,35 @@ import { Fragment, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Divider, Dropdown, Tooltip } from 'antd';
 import { AiOutlineInfoCircle, AiOutlineMenu } from 'react-icons/ai';
-import { CiLight, CiDark } from 'react-icons/ci';
+import { CiDark, CiLight } from 'react-icons/ci';
 import stores from '@/stores';
-import config from '@/data';
-import hooks from '@/hooks';
 
-const Nav = () => {
+const Nav = ({ type = 'default' }) => {
   const { theme, updateTheme } = stores.useConfig((state) => ({
     theme: state.theme,
     updateTheme: state.updateTheme,
   }));
+  const router = useRouter();
 
   return (
     <header className='nav'>
       <div className='title'>
-        sound<span className='emphasize'>swarm</span>
+        {type === 'shared' ? null : (
+          <>
+            sound<span className='emphasize'>swarm</span>
+          </>
+        )}
       </div>
 
       <div className='links'>
         {/* <Links /> */}
         {/* Icons */}
+        {type === 'shared' ? (
+          <>
+            <a onClick={() => router.push('/')}>app</a>
+            <Divider type='vertical' />
+          </>
+        ) : null}
         <Tooltip
           title={
             <>
@@ -45,77 +54,6 @@ const Nav = () => {
       </div>
     </header>
   );
-};
-
-const Links = () => {
-  const { isMobile } = hooks.useWindowSize();
-  const router = useRouter();
-
-  useEffect(() => {
-    const index = config.nav['en'].findIndex(
-      (item) => item === router.pathname.slice(1),
-    );
-
-    if (index !== -1) {
-      setActivePage(config.nav[language][index]);
-    } else if (router.pathname === '/') {
-      setActivePage(config.nav[language][0]);
-    } else {
-      setActivePage('');
-    }
-  }, [router.pathname, setActivePage, language]);
-
-  const items = [
-    {
-      key: '1',
-      label: (
-        <a
-          onClick={() => router.push('/')}
-          className={router.pathname === '/' ? 'active' : ''}>
-          {config.nav[language][0]}
-        </a>
-      ),
-    },
-    {
-      key: '2',
-      label: (
-        <a
-          onClick={() => router.push('/education')}
-          className={router.pathname === '/education' ? 'active' : ''}>
-          {config.nav[language][1]}
-        </a>
-      ),
-    },
-    {
-      key: '3',
-      label: (
-        <a
-          onClick={() => router.push('/about')}
-          className={router.pathname === '/about' ? 'active' : ''}>
-          {config.nav[language][2]}
-        </a>
-      ),
-    },
-  ];
-
-  if (isMobile)
-    return (
-      <>
-        <Dropdown menu={{ items }} trigger={['click']} arrow>
-          <a onClick={(e) => e.preventDefault()}>
-            <AiOutlineMenu size={20} />
-          </a>
-        </Dropdown>
-        <Divider type='vertical' />
-      </>
-    );
-
-  return items.map((item) => (
-    <Fragment key={item.key}>
-      {item.label}
-      <Divider type='vertical' />
-    </Fragment>
-  ));
 };
 
 export default Nav;
